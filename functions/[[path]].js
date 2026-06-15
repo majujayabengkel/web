99a54abb3c314520bcfcbf93c56a348ff2e33c13
majujayabengkel <indahkholidah819@gmail.com>
2026-06-15 08:24:38 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -463,6 +463,7 @@ async function renderPage(c, page) {
         <meta charset='UTF-8'>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${page.title}</title>
+        <link rel="icon" href="/favicon.png">
         <script src="https://cdn.tailwindcss.com"></script>
         <script>${tailwindConfig}</script>
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
